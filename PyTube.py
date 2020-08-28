2d3ca136fc28554940f0b875a9755ae9857b7962
The-Last-Ninja <66144395+The-Last-Ninja@users.noreply.github.com>
2020-08-28 14:49:33 -0400

    Update PyTube.py

--- PyTube.py
+++ PyTube.py
@@ -18,7 +18,7 @@ def exit_prgm(app):
         app.mainloop()
 
 
-# Function for browsing a directory to save a video 
+# Function for browsing a directory to save a video
 def browse(download_path):
 
     download_directory = filedialog.askdirectory(initialdir="Path")
@@ -47,7 +47,7 @@ def main():
 
     app = tk.Tk()
 
-    app.geometry("600x120")
+    app.geometry("525x170")
 
     app.configure(background="Gray")
 
@@ -58,34 +58,40 @@ def main():
     vid_link = StringVar()
 
     download_path = StringVar()
-    
+
     # Set up a menubar for Exit a program
     menubar = Menu(app)
     filemenu = Menu(menubar, tearoff=0)
     filemenu.add_command(label="Exit", command=lambda: exit_prgm(app))
     menubar.add_cascade(label="File", menu=filemenu)
     app.config(menu=menubar)
-    
+
+    # Set up a label for tile and instruction
+    title_txt = Label(app, text="Youtube video downloader", background="Gray", font='Helvetica 20')
+    title_txt.grid(row=0, column=1)
+    instruction = Label(app, text="Paste a video link below to get started", background="Gray", font='Helvetica 14')
+    instruction.grid(row=1, column=1)
+
     # Set up a label and entry box for youtube link
     link_label = Label(app, text="YouTube link  :", background="Gray", font='Arial 12')
-    link_label.grid(row=1, column=0, pady=5, padx=5)
+    link_label.grid(row=2, column=0)
     linktxt = tk.Entry(app, width=55, textvariable=vid_link)
-    linktxt.grid(row=1, column=1, pady=5, padx=5, columnspan=2)
-    
+    linktxt.grid(row=2, column=1)
+
     # Set up a label and entry box for directory destination
     destination = Label(app, text="Destination    :", background="Gray", font='Arial 12')
-    destination.grid(row=2, column=0, pady=5, padx=5)
-    destination_txt = tk.Entry(app, width=40, textvariable=download_path)
-    destination_txt.grid(row=2, column=1, pady=5, padx=5)
-    
-    # Set up a buttons for Browsing a file for download directory and to download youtube video 
+    destination.grid(row=3, column=0)
+    destination_txt = tk.Entry(app, width=55, textvariable=download_path)
+    destination_txt.grid(row=3, column=1)
+
+    # Set up a buttons for Browsing a file for download directory and to download youtube video
     browse_btn = tk.Button(app, text="Browse", command=lambda: browse(download_path), width=10, background="Gray")
-    browse_btn.grid(row=2, column=2, pady=1, padx=1)
+    browse_btn.grid(row=3, column=2)
     download_btn = tk.Button(app, text="Download", command=lambda: download(vid_link, download_path),
                              width=20, background="Gray")
-    download_btn.grid(row=3, column=1, pady=3, padx=3)
-    
-    # Display an app in a loop 
+    download_btn.grid(row=4, column=1)
+
+    # Display an app in a loop
     app.mainloop()
 
 
